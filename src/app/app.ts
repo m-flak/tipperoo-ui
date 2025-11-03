4104830a47a96a1daa616bf5e4187cfe412bcdcc
Matthew Kehrer <matthew@kehrer.dev>
2025-11-03 12:54:56 -0600

    add accordions

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 import { TopBar } from './components/top-bar/top-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBar],
+  imports: [RouterOutlet, TopBar, NgbAccordionModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,4 @@
+import '@angular/localize/init';
 import { Buffer } from 'buffer';
 
 globalThis.Buffer = Buffer;
